ebb40a807d4b3e02b10ce654daec4ddca486cbda
ashlie4 <ashlierenae21@gmail.com>
2026-07-28 20:16:49 -0500

    new updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { StreamfloDetails } from "./streamflo-details/streamflo-details";
 import { SocialmediaListing } from './socialmedia-listing/socialmedia-listing';
 import { SocialmediaOverview } from './socialmedia-overview/socialmedia-overview';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StreamfloDetails, SocialmediaListing, SocialmediaOverview],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
